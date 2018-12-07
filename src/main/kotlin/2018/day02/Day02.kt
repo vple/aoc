a1b7047233df243953f6888812acd845a9f6de3a
Vincent Le <vple@yext.com>
2018-12-07 18:07:37 -0500

    Copy over 2018 solutions.

--- src/main/kotlin/2018/day02/Day02.kt
+++ src/main/kotlin/2018/day02/Day02.kt
@@ -3,15 +3,83 @@ package `2018`.day02
 import util.loadResource
 
 /**
- * [Advent of Code 2018 Day 2](https://adventofcode.com/2018/day/2)
+ * Solutions for [Advent of Code Day 2](https://adventofcode.com/2018/day/2).
  */
 
 const val INPUT_FILE = "/day02/input.txt"
 
 fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE)
+    val boxIds = input.lines().filterNot { it.isEmpty() }
 
     println("Part 1")
+    val checksum = computeChecksum(boxIds)
+    println(checksum)
 
     println("Part 2")
+    val commonLetters = findCommonLetters(boxIds)
+    println(commonLetters)
 }
+
+/**
+ * Computes a checksum for the given [boxIds].
+ */
+fun computeChecksum(boxIds: List<String>): Int {
+    val twoLetterBoxCount = boxIds.filter { it.containsExactly2() }.count()
+    val threeLetterBoxCount = boxIds.filter { it.containsExactly3() }.count()
+    return twoLetterBoxCount * threeLetterBoxCount
+}
+
+/**
+ * Counts the number of times each letter occurs in the given [boxId].
+ */
+fun countLetters(boxId: String): Map<Char, Int> {
+    return boxId.groupingBy { it }.eachCount()
+}
+
+/**
+ * Returns true if this string contains exactly [n] of any letter.
+ */
+fun String.containsExactlyN(n: Int): Boolean {
+    return countLetters(this).values.contains(n)
+}
+
+/**
+ * Returns true if this string contains exactly 2 of any letter.
+ */
+fun String.containsExactly2() = this.containsExactlyN(2)
+
+/**
+ * Returns true if this string contains exactly 3 of any letter.
+ */
+fun String.containsExactly3() = this.containsExactlyN(3)
+
+/**
+ * Finds all common letters between any pair of [boxIds] that differ by one letter.
+ *
+ * Requires that all box IDs are the same length.
+ */
+fun findCommonLetters(boxIds: List<String>): Set<String> {
+    val idLength = boxIds.map { it.count() }.toSet().single()
+    val differingIndex = (0 until idLength).first { hasSimilarIdsAt(it, boxIds) }
+    val commonLetters =
+        boxIds.map { it.removeAt(differingIndex) }
+            .groupingBy { it }
+            .eachCount()
+            .filter { it.value > 1 }
+            .keys
+    return commonLetters
+}
+
+/**
+ * Returns true if the [boxIds] contains two IDs that differ exactly at [index].
+ */
+fun hasSimilarIdsAt(index: Int, boxIds: List<String>): Boolean {
+    val boxIdsWithoutIndex = boxIds.map { it.removeAt(index) }.toSet()
+    return boxIdsWithoutIndex.count() != boxIds.count()
+}
+
+/**
+ * Removes the character at the given [index], returning the result.
+ */
+fun String.removeAt(index: Int) = this.removeRange(index..index)
--- src/main/kotlin/2018/day07/Day07.kt
+++ src/main/kotlin/2018/day07/Day07.kt
@@ -3,15 +3,81 @@ package `2018`.day07
 import util.loadResource
 
 /**
- * [Advent of Code 2018 Day 7](https://adventofcode.com/2018/day/7)
+ * [Advent of Code 2017 Day 7](https://adventofcode.com/2017/day/7)
  */
 
 const val INPUT_FILE = "/day07/input.txt"
 
 fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE)
+    val lines = input.lines().filterNot { it.isEmpty() }
+    val regex = Regex("Step (\\w) must be finished before step (\\w) can begin.")
+    val dependencies = lines
+        .mapNotNull { regex.matchEntire(it)?.groupValues }
+        .map { Pair(it[1].first(), it[2].first()) }
 
     println("Part 1")
+    val singleWorkerSchedule = schedule(dependencies, 1)
+    val steps = singleWorkerSchedule.toList().sortedBy { (step, start) -> start }.map { (step, _) -> step }
+    println(steps.joinToString(separator = ""))
 
     println("Part 2")
+    val fiveWorkerSchedule = schedule(dependencies, 5, ::stepTime)
+    val completionTime = fiveWorkerSchedule.map { (step, start) -> start + stepTime(step) }.max()
+    println(completionTime)
 }
+
+/**
+ * Schedules the steps in the given [dependencies], using [numWorkers] workers and with each step taking the amount of time given by [stepTime].
+ * Returns the start times for each step.
+ */
+fun schedule(
+    dependencies: List<Pair<Char, Char>>,
+    numWorkers: Int = 1,
+    stepTime: (Char) -> Int = { 1 }
+): Map<Char, Int> {
+    return scheduleHelper(
+        dependencies = dependencies,
+        allSteps = dependencies.map { listOf(it.first, it.second) }.flatten().toSet(),
+        numWorkers = numWorkers,
+        stepTime = stepTime,
+        currentTime = 0)
+}
+
+/**
+ * Recursive helper function to schedule the [dependencies].
+ */
+fun scheduleHelper(
+    schedule: Map<Char, Int> = mapOf(), // Step to start time
+    dependencies: List<Pair<Char, Char>>,
+    allSteps: Set<Char>,
+    numWorkers: Int,
+    stepTime: (Char) -> Int,
+    currentTime: Int = 0
+): Map<Char, Int> {
+    val completedSteps = schedule.filter { (step, start) -> start + stepTime(step) <= currentTime }.keys
+    val currentSteps = schedule.filterKeys { !completedSteps.contains(it) }.keys
+    val remainingSteps = allSteps - completedSteps - currentSteps
+
+    if (remainingSteps.isEmpty()) {
+        return schedule
+    }
+
+    val remainingDependencies = dependencies.filterNot { (step, _) -> completedSteps.contains(step) }
+    val availableSteps = remainingSteps - remainingDependencies.map { it.second }
+    val availableWorkers = numWorkers - currentSteps.size
+    val steps = availableSteps.sorted().take(availableWorkers)
+    val nextSchedule = schedule + steps.map { Pair(it, currentTime) }
+
+    val nextCompletedStepTime = nextSchedule
+        .filterKeys { currentSteps.contains(it) || steps.contains(it) }
+        .map { (step, start) -> start + stepTime(step) }
+        .min()!!
+
+    return scheduleHelper(nextSchedule, dependencies, allSteps, numWorkers, stepTime, nextCompletedStepTime)
+}
+
+/**
+ * The amount of time it takes to complete [step].
+ */
+fun stepTime(step: Char) = step - 'A' + 60 + 1
