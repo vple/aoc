3d60df2b08ea67b8c7ffaae3aff18958ad8f844b
Vincent Le <vple@yext.com>
2018-12-08 15:18:53 -0500

    Factor out KnotHash.

--- src/main/kotlin/aoc2017/day10/Day10.kt
+++ src/main/kotlin/aoc2017/day10/Day10.kt
@@ -11,34 +11,13 @@ const val INPUT_FILE = "/aoc2017/day10/input.txt"
 fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE).trim()
     val part1Lengths = input.split(",").map { it.toInt() }
-    val part2Lengths = fullKnotHashLengths(input)
 
     println("Part 1")
     val hash = knotHash(part1Lengths)
     println(hash[0] * hash[1])
 
     println("Part 2")
-    val sparseHash = knotHash(part2Lengths.asIterable())
-    val denseHash = denseHash(sparseHash)
-    println(encode(denseHash))
-}
-
-fun encode(denseHash: List<Int>): String {
-    return denseHash.map { it.toString(16) }.map { it.padStart(2, '0') }.joinToString("")
-}
-
-fun denseHash(sparseHash: List<Int>): List<Int> {
-    val denseHash = mutableListOf<Int>()
-    var counter = 0
-    var xorTotal = 0
-    for (element in sparseHash) {
-        xorTotal = xorTotal xor element
-        if (++counter % 16 == 0) {
-            denseHash += xorTotal
-            xorTotal = 0
-        }
-    }
-    return denseHash
+    println(KnotHash(input).hash)
 }
 
 fun knotHash(lengths: Iterable<Int>): MutableList<Int> {
@@ -59,12 +38,4 @@ fun reverse(knot: MutableList<Int>, position: Int, length: Int) {
     val indices = (position until position+length).map { it % knot.size }
     val reversed = indices.zip(indices.map { knot[it] }.reversed()).toMap()
     indices.forEach { knot[it] = reversed[it]!! }
-}
-
-fun fullKnotHashLengths(string: String): Sequence<Int> {
-    val base = string.map { it.toInt() } + listOf(17, 31, 73, 47, 23)
-    var i = 0
-    return generateSequence {
-        base.asSequence().takeIf { i++ < 64 }
-    }.flatten()
 }
---
+++ src/main/kotlin/aoc2017/day10/KnotHash.kt
@@ -0,0 +1,46 @@
+package aoc2017.day10
+
+/**
+ * The Knot Hash for a given [input].
+ */
+data class KnotHash(val input: String) {
+    val lengths: List<Int> by lazy { input.map { it.toInt() } + listOf(17, 31, 73, 47, 23) }
+    val fullLengths: Sequence<Int> by lazy {
+        var i = 0
+        generateSequence { lengths.asSequence().takeIf { i++ < 64 } }.flatten()
+    }
+    val sparseHash: List<Int> by lazy {
+        val knot = MutableList(256) { it }
+        val size = knot.size
+        var position = 0
+        var skip = 0
+        for (length in fullLengths) {
+            reverse(knot, position, length)
+            position = (position + length + skip) % size
+            skip++
+        }
+        knot
+    }
+    val denseHash by lazy {
+        val denseHash = mutableListOf<Int>()
+        var counter = 0
+        var xorTotal = 0
+        for (element in sparseHash) {
+            xorTotal = xorTotal xor element
+            if (++counter % 16 == 0) {
+                denseHash += xorTotal
+                xorTotal = 0
+            }
+        }
+        denseHash.toList()
+    }
+    val hash by lazy {
+        denseHash.map { it.toString(16) }.map { it.padStart(2, '0') }.joinToString("")
+    }
+
+    private fun reverse(knot: MutableList<Int>, position: Int, length: Int) {
+        val indices = (position until position+length).map { it % knot.size }
+        val reversed = indices.zip(indices.map { knot[it] }.reversed()).toMap()
+        indices.forEach { knot[it] = reversed[it]!! }
+    }
+}
