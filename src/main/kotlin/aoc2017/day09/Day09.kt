9c71189c5684c20202785f9ebca4d6fe41dd400f
Vincent Le <vple@yext.com>
2018-12-07 22:44:33 -0500

    2017/09 solutions.

--- src/main/kotlin/aoc2017/day09/Day09.kt
+++ src/main/kotlin/aoc2017/day09/Day09.kt
@@ -9,9 +9,47 @@ import util.loadResource
 const val INPUT_FILE = "/aoc2017/day09/input.txt"
 
 fun main(args: Array<String>) {
-    val input = loadResource(INPUT_FILE)
+    val stream = loadResource(INPUT_FILE).trim()
 
+    val result = processStream(stream)
     println("Part 1")
+    println(result.first)
 
     println("Part 2")
+    println(result.second)
+}
+
+fun processStream(stream: String): Pair<Int, Int> {
+    var depth = 0
+    var score = 0
+    var garbageCount = 0
+    var inGarbage = false
+    var ignoreNext = false
+    for (char in stream) {
+        if (ignoreNext) {
+            ignoreNext = false
+            continue
+        }
+        if (char == '!') {
+            ignoreNext = true
+            continue
+        }
+        if (inGarbage && char != '>') {
+            garbageCount++
+            continue
+        }
+        when (char) {
+            '<' -> inGarbage = true
+            '>' -> inGarbage = false
+            '{' -> {
+                depth++
+            }
+            '}' -> {
+                score += depth
+                depth--
+            }
+            else -> {}
+        }
+    }
+    return Pair(score, garbageCount)
 }
