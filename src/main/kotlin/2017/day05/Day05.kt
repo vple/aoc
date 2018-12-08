e2bad0d5e550a634cfa658dd796020870ca85c86
Vincent Le <vple@yext.com>
2018-12-07 19:14:08 -0500

    Fix 2017 solutions.

--- src/main/kotlin/2017/day05/Day05.kt
+++ src/main/kotlin/2017/day05/Day05.kt
@@ -6,12 +6,33 @@ import util.loadResource
  * [Advent of Code 2017 Day 5](https://adventofcode.com/2017/day/5)
  */
 
-const val INPUT_FILE = "/day05/input.txt"
+const val INPUT_FILE = "/2017/day05/input.txt"
 
 fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE)
+    val offsets = input.lines().filterNot { it.isEmpty() }.map { it.toInt() }
 
     println("Part 1")
+    println(jump(offsets) { it+1 })
 
     println("Part 2")
+    println(jump(offsets) { if (it >= 3) { it-1 } else { it+1 } })
+}
+
+/**
+ * Jumps until the exit is reached, using the given [offsets]. Every time an offset is visited, it is changed according to [modify].
+ */
+fun jump(offsets: List<Int>, modify: (Int) -> Int): Int {
+    val mutableOffsets = offsets.toMutableList()
+    var index = 0
+    var steps = 0
+
+    while (index >= 0 && index < offsets.size) {
+        val offset = mutableOffsets[index]
+        mutableOffsets[index] = modify(offset)
+        index += offset
+        steps++
+    }
+
+    return steps
 }
