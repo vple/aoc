eb6a9b3afcbb2cf9a7a8def8ec4563d16b294c1c
Vincent Le <vple@yext.com>
2018-12-07 20:54:16 -0500

    2017/07 solution.

---
+++ src/main/kotlin/2017/day07/Program.kt
@@ -0,0 +1,40 @@
+package `2017`.day07
+
+/**
+ * A program.
+ */
+data class Program(val name: String, val weight: Int, val subprogramNames: Set<String> = setOf()) {
+    /**
+     * Returns true if this program is balanced.
+     */
+    fun isBalanced(totalWeights: Map<String, Int>): Boolean {
+        if (subprogramNames.isEmpty()) {
+            return true
+        }
+
+        return subprogramNames.map { totalWeights[it] }.toSet().size == 1
+    }
+
+    companion object {
+        const val PROGRAM_PATTERN = "(\\w+) \\((\\d+)\\)"
+        const val SUBPROGRAM_PATTERN = "-> (.+)"
+        val PROGRAM_REGEX = Regex(PROGRAM_PATTERN)
+        val SUBPROGRAM_REGEX = Regex(SUBPROGRAM_PATTERN)
+
+        fun parse(program: String): Program {
+            val programValues = PROGRAM_REGEX.find(program)!!.groupValues
+            val name = programValues[1]
+            val weight = programValues[2].toInt()
+
+            val subprogramValues = SUBPROGRAM_REGEX.find(program)?.groupValues
+            val subprogramNames =
+                if (subprogramValues == null) {
+                    setOf()
+                } else {
+                    subprogramValues[1].split(',').map { it.trim() }.toSet()
+                }
+
+            return Program(name, weight, subprogramNames)
+        }
+    }
+}
