9494ae543666a51234b55a55bb367b67a4f91551
Vincent Le <vple@yext.com>
2018-12-09 17:42:02 -0500

    2015/04 solutions.

--- src/main/kotlin/aoc2015/day04/Day04.kt
+++ src/main/kotlin/aoc2015/day04/Day04.kt
@@ -1,17 +1,39 @@
 package aoc2015.day04
 
 import util.loadResource
+import java.math.BigInteger
+import java.security.MessageDigest
 
 /**
  * [Advent of Code 2015 Day 4](https://adventofcode.com/2015/day/4)
  */
 
 const val INPUT_FILE = "/aoc2015/day04/input.txt"
+val MD5 = MessageDigest.getInstance("MD5")
 
 fun main(args: Array<String>) {
-    val input = loadResource(INPUT_FILE)
+    val input = "bgvyzdsv"
 
     println("Part 1")
+    println(findHash(input, "00000"))
 
     println("Part 2")
+    println(findHash(input, "000000"))
+}
+
+fun findHash(prefix: String, hashPrefix: String): Pair<Int, String> {
+    var n = 0
+    var hash: String
+    do {
+        n++
+        hash = hash(prefix, n)
+    } while (!hash.startsWith(hashPrefix))
+    return Pair(n, hash)
+}
+
+fun hash(prefix: String, number: Int): String {
+    val input = prefix + number
+    return MD5.digest(input.toByteArray()).joinToString("") {
+        String.format("%02x", it)
+    }
 }
