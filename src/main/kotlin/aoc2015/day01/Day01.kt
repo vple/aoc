be064fb9ede1f6919ae34d49d65f61de4a537d10
Vincent Le <vple@yext.com>
2018-12-09 17:14:13 -0500

    2015/01 solutions.

--- src/main/kotlin/aoc2015/day01/Day01.kt
+++ src/main/kotlin/aoc2015/day01/Day01.kt
@@ -9,9 +9,23 @@ import util.loadResource
 const val INPUT_FILE = "/aoc2015/day01/input.txt"
 
 fun main(args: Array<String>) {
-    val input = loadResource(INPUT_FILE)
+    val input = loadResource(INPUT_FILE).trim()
 
     println("Part 1")
+    val floor = input.count { it == '(' } - input.count { it == ')'}
+    println(floor)
 
     println("Part 2")
+    println(enterBasement(input))
+}
+
+fun enterBasement(parens: String): Int {
+    var floor = 0
+    parens.forEachIndexed { index, c ->
+        floor+= if (c == '(') 1 else -1
+        if (floor < 0) {
+            return index+1
+        }
+    }
+    throw IllegalArgumentException()
 }
