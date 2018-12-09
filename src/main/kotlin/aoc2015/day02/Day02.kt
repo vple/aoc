41ba95347243809320ed1ec6a71723bffe83c7b0
Vincent Le <vple@yext.com>
2018-12-09 17:19:27 -0500

    2015/02 solutions.

--- src/main/kotlin/aoc2015/day02/Day02.kt
+++ src/main/kotlin/aoc2015/day02/Day02.kt
@@ -1,6 +1,6 @@
 package aoc2015.day02
 
-import util.loadResource
+import util.loadResourceLines
 
 /**
  * [Advent of Code 2015 Day 2](https://adventofcode.com/2015/day/2)
@@ -9,9 +9,28 @@ import util.loadResource
 const val INPUT_FILE = "/aoc2015/day02/input.txt"
 
 fun main(args: Array<String>) {
-    val input = loadResource(INPUT_FILE)
+    val input = loadResourceLines(INPUT_FILE)
+    val boxes = input.map { Box.parse(it) }
 
     println("Part 1")
+    println(boxes.map { it.wrappingPaperRequired }.sum())
 
     println("Part 2")
+    println(boxes.map { it.ribbonRequired }.sum())
+}
+
+data class Box(val length: Int, val width: Int, val height: Int) {
+    val sideAreas = listOf(length * width, width * height, height * length)
+    val volume = length * width * height
+    val perimeters = listOf(2 * (length + width), 2 * (width + height), 2 * (height + length))
+    val wrappingPaperRequired = 2 * sideAreas.sum() + sideAreas.min()!!
+    val ribbonRequired = perimeters.min()!! + volume
+
+    companion object {
+        val REGEX = Regex("(\\d+)x(\\d+)x(\\d+)")
+        fun parse(text: String): Box {
+            val (_, l, w, h) = REGEX.matchEntire(text)!!.groupValues
+            return Box(l.toInt(), w.toInt(), h.toInt())
+        }
+    }
 }
