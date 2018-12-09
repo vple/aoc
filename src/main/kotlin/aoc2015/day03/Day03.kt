f11fed5c48def798eea40828d1cff43f7565908c
Vincent Le <vple@yext.com>
2018-12-09 17:31:26 -0500

    2015/03 solutions.

--- src/main/kotlin/aoc2015/day03/Day03.kt
+++ src/main/kotlin/aoc2015/day03/Day03.kt
@@ -1,5 +1,7 @@
 package aoc2015.day03
 
+import grid.Coordinate
+import grid.Direction
 import util.loadResource
 
 /**
@@ -9,9 +11,38 @@ import util.loadResource
 const val INPUT_FILE = "/aoc2015/day03/input.txt"
 
 fun main(args: Array<String>) {
-    val input = loadResource(INPUT_FILE)
+    val input = loadResource(INPUT_FILE).trim().toList()
+    val directions = input.map {
+        when (it) {
+            '^' -> Direction.NORTH
+            '>' -> Direction.EAST
+            'v' -> Direction.SOUTH
+            '<' -> Direction.WEST
+            else -> throw IllegalStateException()
+        }
+    }
 
     println("Part 1")
+    val deliveries = deliver(directions)
+    println(deliveries.size)
 
     println("Part 2")
+    val allDeliveries = mutableMapOf<Coordinate, Int>()
+    allDeliveries.putAll(deliver(directions.filterIndexed { index, direction -> index % 2 == 0 }))
+    val roboDeliveries= deliver(directions.filterIndexed { index, direction -> index % 2 == 1 })
+    roboDeliveries.forEach { allDeliveries.merge(it.key, it.value) { x, y -> x + y } }
+    println(allDeliveries.size)
+}
+
+fun deliver(directions: List<Direction>): Map<Coordinate, Int> {
+    val deliveries = mutableMapOf<Coordinate, Int>()
+    var current = Coordinate(0, 0)
+    deliveries[current] = deliveries.getOrDefault(current, 0) + 1
+
+    directions.forEach {
+        current += it.coordinate
+        deliveries[current] = deliveries.getOrDefault(current, 0) + 1
+    }
+
+    return deliveries
 }
