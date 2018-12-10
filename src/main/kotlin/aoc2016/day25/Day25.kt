c2243777cfbf469ffc151a9da475848213766fc1
Vincent Le <vple@yext.com>
2018-12-09 21:56:20 -0500

    2016/25 solution.

--- src/main/kotlin/aoc2016/day25/Day25.kt
+++ src/main/kotlin/aoc2016/day25/Day25.kt
@@ -12,6 +12,36 @@ fun main(args: Array<String>) {
     val input = loadResource(INPUT_FILE)
 
     println("Part 1")
+    val start = search(643*4)
+    println(start)
+    signal(start)
 
     println("Part 2")
+}
+
+fun search(offset: Int): Int {
+    // 0b10
+    var current = 2
+    while (current <= offset) {
+        // Append 0b10
+        current *= 4
+        current += 2
+    }
+    return current - offset
+}
+
+fun signal(start: Int) {
+    var a = start
+//    while (true) {
+        a = start + (643*4)
+        while (a != 0) {
+            a = a / 2
+            if (a % 2 == 0) {
+                print(0)
+            } else {
+                print(1)
+            }
+        }
+//    }
+    println()
 }
