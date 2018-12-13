0bf3ee02882ca06ecf737c9e781907495527e10f
Vincent Le <vple@yext.com>
2018-12-12 23:51:00 -0500

    Add results to graph searches.

--- src/main/kotlin/util/Graph.kt
+++ src/main/kotlin/util/Graph.kt
@@ -9,7 +9,8 @@ data class SearchState<T>(
     val seen: Set<T> = setOf(),
     val level: Int = 0,
     val iterations: Int = 0,
-    val found: Boolean = false
+    val found: Boolean = false,
+    val result: T? = null
 )
 
 /**
@@ -20,14 +21,20 @@ data class SearchState<T>(
 fun <T> breadthFirstSearch(
     start: Iterable<T>,
     seen: Set<T> = setOf(),
-    target: T? = null,
+    target: ((T) -> Boolean)? = null,
+    finishLevel: Boolean = false,
     adjacent: (T) -> Iterable<T>
 ): SearchState<T> {
     val initial = SearchState(start.toList(), seen)
-    return breadthFirstSearch(initial, target, adjacent)
+    return breadthFirstSearch(initial, target, finishLevel, adjacent)
 }
 
-private fun <T> breadthFirstSearch(state: SearchState<T>, target: T?, adjacent: (T) -> Iterable<T>): SearchState<T> {
+private fun <T> breadthFirstSearch(
+    state: SearchState<T>,
+    target: ((T) -> Boolean)?,
+    finishLevel: Boolean,
+    adjacent: (T) -> Iterable<T>
+): SearchState<T> {
     if (state.current.isEmpty()) {
         return state
     }
@@ -35,22 +42,33 @@ private fun <T> breadthFirstSearch(state: SearchState<T>, target: T?, adjacent: 
     val seen = state.seen.toMutableSet()
     var iterations = state.iterations
     val next = linkedSetOf<T>()
+    var match: SearchState<T>? = null
     state.current.forEachIndexed { index, node ->
         seen.add(node)
         iterations++
-        if (node == target) {
-            return SearchState(
-                state.current.subList(index+1, state.current.size),
-                seen,
-                state.level,
-                iterations,
-                true)
+        if (target != null && target(node)) {
+            val result =
+                SearchState(
+                    state.current.subList(index + 1, state.current.size),
+                    seen,
+                    state.level,
+                    iterations,
+                    true,
+                    node)
+            if (!finishLevel) {
+                return result
+            } else if (match == null) {
+                match = result
+            }
         }
         next += adjacent(node).filterNot { seen.contains(it) }
     }
+    match?.let {
+        return it.copy(listOf(), seen, iterations = iterations)
+    }
 
     val nextState = SearchState(next.toList(), seen, state.level + 1, iterations)
-    return breadthFirstSearch(nextState, target, adjacent)
+    return breadthFirstSearch(nextState, target, finishLevel, adjacent)
 }
 
 /**
@@ -61,14 +79,18 @@ private fun <T> breadthFirstSearch(state: SearchState<T>, target: T?, adjacent: 
 fun <T> depthFirstSearch(
     start: Iterable<T>,
     seen: Set<T> = setOf(),
-    target: T? = null,
+    target: ((T) -> Boolean)?,
     adjacent: (T) -> Iterable<T>
 ): SearchState<T> {
     val initial = SearchState(start.toList(), seen)
     return depthFirstSearch(initial, target, adjacent)
 }
 
-private fun <T> depthFirstSearch(state: SearchState<T>, target: T?, adjacent: (T) -> Iterable<T>): SearchState<T> {
+private fun <T> depthFirstSearch(
+    state: SearchState<T>,
+    target: ((T) -> Boolean)?,
+    adjacent: (T) -> Iterable<T>
+): SearchState<T> {
     val seen = state.seen.toMutableSet()
     var current = state.current.filterNot { seen.contains(it) }
     if (current.isEmpty()) {
@@ -77,13 +99,14 @@ private fun <T> depthFirstSearch(state: SearchState<T>, target: T?, adjacent: (T
 
     val node = current.first()
     seen.add(node)
-    if (node == target) {
+    if (target != null && target(node)) {
         return SearchState(
             current.subList(1, current.size),
             seen,
             state.level,
             state.iterations + 1,
-            true)
+            true,
+            node)
     }
     current = adjacent(node).filterNot { seen.contains(it) } + current
     val nextState = SearchState(current, seen, state.level, state.iterations + 1)
